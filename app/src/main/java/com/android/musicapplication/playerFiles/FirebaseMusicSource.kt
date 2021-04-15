ace0fbd205d58cd9abae434a039886de249588f3
Solo4 <akasha02@mail.ru>
2021-04-15 19:59:09 +0300

    state music checking class

---
+++ app/src/main/java/com/android/musicapplication/playerFiles/FirebaseMusicSource.kt
@@ -0,0 +1,41 @@
+package com.android.musicapplication.playerFiles
+
+import com.android.musicapplication.playerFiles.State.*
+
+class FirebaseMusicSource {
+
+    private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
+
+    private var state: State = STATE_CREATED
+        set(value) {
+            if (value == STATE_INITIALIZED || value == STATE_ERROR) {
+                synchronized(onReadyListeners) {
+                    field = value
+                    onReadyListeners.forEach { listener ->
+                        listener(state == STATE_INITIALIZED)
+                    }
+                }
+            } else {
+                field = value
+            }
+        }
+
+    fun whenReady(action: (Boolean) -> Unit) =
+        if (state == STATE_CREATED || state == STATE_INITIALIZING) {
+            onReadyListeners += action
+            false
+        } else {
+            action(state == STATE_INITIALIZED)
+            true
+        }
+
+
+}
+
+enum class State {
+    STATE_CREATED,
+    STATE_ERROR,
+    STATE_INITIALIZED,
+    STATE_INITIALIZING
+
+}
